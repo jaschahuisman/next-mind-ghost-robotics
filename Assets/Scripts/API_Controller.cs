be2c7bf2c35f414824fa33548c077971d37dbf2e
Jascha Huisman <jascha.huisman@gmail.com>
2021-05-29 10:28:45 +0200

    sit – added sit method to api controller

--- Assets/Scripts/API_Controller.cs
+++ Assets/Scripts/API_Controller.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
+
 
 public class API_Controller : MonoBehaviour
 {
+    public string URI = "http://192.168.168.101";
+    public int PORT = 1234;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    void Sit()
     {
-        
+        string uri = URI + "/sit";
+        using (UnityWebRequest request = UnityWebRequest.Get(uri))
+        {
+            yield return request.SendWebRequest();
+            if (request.isNetworkError || request.isHttpError)
+            {
+                Debug.log("Network/http error: " + request);
+            }
+        }
     }
 }
